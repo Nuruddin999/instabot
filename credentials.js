4d762831dffe8b3de7bd8cfd219e5cd2b6b4a174
Nuruddin <sg7720@gmail.com>
2019-11-08 18:04:29 +0300

    delete index

--- credentials.js
+++ credentials.js
@@ -1,6 +1,4 @@
 module.exports = {
-  username: "mir_serebra_",
-  password: "serebronur9999",
-  usernamenozhi: "nozhi999",
-  passwordnozhi: "nurnozh999"
+  usernamenozhi: "<enter here your login>",
+  passwordnozhi: "<enter here your password>"
 };
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Instabot
+
+## Bot who floolws on every user from gathered list
+
+Made on Puppeteer
+Glad to usefull commits
